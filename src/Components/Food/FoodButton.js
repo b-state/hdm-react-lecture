450c6b701677a1c95ee898259171a319497cecbc
Isabel Mötsch <isi103@web.de>
2023-05-01 12:58:13 +0200

    Redux Excercise still with errors

--- src/Components/Food/FoodButton.js
+++ src/Components/Food/FoodButton.js
@@ -1,20 +1,34 @@
 import React, {useCallback} from 'react'
 import './FoodButton.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { updateCategory } from '../../Actions'
 
-const FoodButton = ({buttonName, foodCategorie, onCategoryChange}) => {
+const FoodButton = ({buttonName, foodCategorie}) => {
 
+    const dispatch = useDispatch();
+    /*
     const onButtonChange= (category) => {
         onCategoryChange(category)
+    } 
+    */
+
+   const category = useSelector(state => state.categoryReducer.type);
+
+
+    const changeCategory = () => {
+        // hier fehlt noch change der category je nach button
+        const newCategory = 'pizza';
+        dispatch(updateCategory(newCategory));
     }
 
+    // Hier onclick rausnehmen
     return (
-        <button className="food-buttons" onClick={onButtonChange} value={foodCategorie}>
+        <button className="food-buttons" onClick={changeCategory} value={foodCategorie}>
             {buttonName}
         </button>
     )
 };
 
 export default FoodButton
-
 
 
